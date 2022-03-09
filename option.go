c88dc8a8a318035471be8c7d946f8bba3c204daa
Jim <jlambert@hashicorp.com>
2022-03-09 18:18:36 -0500

    feature: support WithWriter option for PrettyPrint

--- option.go
+++ option.go
@@ -1,5 +1,10 @@
 package gldap
 
+import (
+	"io"
+	"reflect"
+)
+
 // Option defines a common functional options type which can be used in a
 // variadic parameter pattern.
 type Option func(interface{})
@@ -13,4 +18,40 @@ func applyOpts(opts interface{}, opt ...Option) {
 		}
 		o(opts)
 	}
+}
+
+type generalOptions struct {
+	withWriter io.Writer
+}
+
+func generalDefaults() generalOptions {
+	return generalOptions{}
+}
+
+func getGeneralOpts(opt ...Option) generalOptions {
+	opts := generalDefaults()
+	applyOpts(&opts, opt...)
+	return opts
+}
+
+// WithWriter allows you to specify an optional writer.
+func WithWriter(w io.Writer) Option {
+	return func(o interface{}) {
+		if o, ok := o.(*generalOptions); ok {
+			if !isNil(w) {
+				o.withWriter = w
+			}
+		}
+	}
+}
+
+func isNil(i interface{}) bool {
+	if i == nil {
+		return true
+	}
+	switch reflect.TypeOf(i).Kind() {
+	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
+		return reflect.ValueOf(i).IsNil()
+	}
+	return false
 }
